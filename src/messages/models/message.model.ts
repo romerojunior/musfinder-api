00ea3a92d323cc5ed98090489b1442ab424fad12
Romero <romerojnr@gmail.com>
2020-10-14 23:48:52 +0200

    add message model

---
+++ src/messages/models/message.model.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class Message {
+  @ApiProperty()
+  guid?: string;
+
+  @ApiProperty()
+  to: string;
+
+  @ApiProperty()
+  from: string;
+
+  @ApiProperty()
+  content: string;
+
+  @ApiProperty()
+  createdAt: string;
+
+  @ApiProperty()
+  updatedAt: string;
+
+  @ApiProperty()
+  readAt: string;
+}
